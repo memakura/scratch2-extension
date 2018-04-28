c3b83090d38ab753c64ff3daf102047eefb05b01
memakura <memakura@gmail.com>
2018-04-29 07:25:17 +0900

    Add Tck/Tk path

--- setup.py
+++ setup.py
@@ -2,6 +2,12 @@
 
 from cx_Freeze import setup, Executable
 
+# --- for resolving KeyError: 'TCL_LIBRARY' ---
+import os.path
+PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))
+os.environ['TCL_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tcl8.6')
+os.environ['TK_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tk8.6')
+# ------
 
 name = "s2extest"
 version = "1.0.0"
